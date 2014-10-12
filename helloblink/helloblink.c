eed3032c22515e8dfb416c182df7f7bfd9492d3e
Anton Eliasson <devel@antoneliasson.se>
2014-10-12 21:33:42 +0200

    Add LED blink program

---
+++ helloblink/helloblink.c
@@ -0,0 +1,16 @@
+#define F_CPU 1000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+    DDRC = 0b00111100;
+
+    while (1) {
+        PORTC |= 1<<5 | 1<<4 | 1<<3 | 1<<2;
+        _delay_ms(500);
+        PORTC &= ~(1<<5 | 1<<4 | 1<<3 | 1<<2);
+        _delay_ms(500);
+    }
+}
