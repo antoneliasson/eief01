d5738619c9d2cf6baf6c34cf1496731457ec5bff
Anton Eliasson <devel@antoneliasson.se>
2014-11-15 15:57:59 +0100

    First attempt at incremental encoder decoding

--- dimmer/dimmer.c
+++ dimmer/dimmer.c
@@ -5,8 +5,11 @@
 #include "dimmer.h"
 #include "pinfunctions.h"
 
-#define BAUDRATE 4800
+#define BAUDRATE 38400 // practical max with 8 MHz clock
 #define UBRR F_CPU / BAUDRATE / 16 - 1
+
+volatile unsigned char AB_old;
+volatile unsigned char counter;
 
 static void init_usart(unsigned int ubrr)
 {
@@ -33,22 +36,50 @@ static void init_pwm(void)
     TCCR0A = 1<<COM0A1 | 1<<WGM01 | 1<<WGM00;
 }
 
+static void init_pci(void)
+{
+    // Enable pin change interrupt on PCINT7..0
+    PCICR = 1<<PCIE0;
+    // Enable pin change interrupt on PCINT1, PCINT0
+    PCMSK0 = 1<<PCINT1 | 1<<PCINT0;
+}
+
 int main(void)
 {
     DDRC = 1<<5 | 1<<2;
+    PORTB = 1<<2 | 1<<1; // enable pullups
 
     init_usart(UBRR);
     init_pwm();
+    init_pci();
+
     OCR0A = 0x40; // 25 % duty cycle
     sei();
 
+    unsigned char old_counter = 0;
     while (1) {
-        PORTC |= 1<<2;
-        _delay_ms(500);
-        PORTC &= ~(1<<2);
+        if (old_counter != counter) {
+            UDR0 = counter;
+            old_counter = counter;
+        }
+        toggle_heartbeat_led();
         _delay_ms(500);
     }
     return 0;
+}
+
+static void incCounter(void)
+{
+    if (counter < 255) {
+        counter += 1;
+    }
+}
+
+static void decCounter(void)
+{
+    if (counter > 0) {
+        counter -= 1;
+    }
 }
 
 ISR(USART_RX_vect)
@@ -56,4 +87,18 @@ ISR(USART_RX_vect)
     toggle_status_led();
     char received = UDR0;
     UDR0 = received; // echo it back
+}
+
+ISR(PCINT0_vect)
+{
+    // René Sommer's algorithm
+    unsigned char BA_new = PINB>>1 & 3;
+    unsigned char sum = AB_old ^ BA_new;
+    if (sum == 1) {
+        incCounter();
+    } else if (sum == 2) {
+        decCounter();
+    }
+    // swap bits A and B
+    AB_old = (BA_new>>1 & 1) | (BA_new<<1 & 2);
 }
