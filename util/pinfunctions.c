384f5b0f8ca4c17fb9b71eed268dddb1a481fce2
Anton Eliasson <devel@antoneliasson.se>
2014-11-15 15:57:18 +0100

    Add heartbeat blinking function

--- util/pinfunctions.c
+++ util/pinfunctions.c
@@ -9,3 +9,12 @@ void toggle_status_led(void)
         PORTC |= 1<<PC5;
     }
 }
+
+void toggle_heartbeat_led(void)
+{
+    if (PORTC & 1<<PC2) {
+        PORTC &= ~(1<<PC2);
+    } else {
+        PORTC |= 1<<PC2;
+    }
+}
--- util/pinfunctions.h
+++ util/pinfunctions.h
@@ -1,3 +1,4 @@
 #pragma once
 
 void toggle_status_led(void);
+void toggle_heartbeat_led(void);
