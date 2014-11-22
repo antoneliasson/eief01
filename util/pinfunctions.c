e59784ec27a14bf06e98aa96d9cf8873ff4c0364
Anton Eliasson <devel@antoneliasson.se>
2014-11-22 22:22:35 +0100

    More status LEDs!

--- util/pinfunctions.c
+++ util/pinfunctions.c
@@ -10,6 +10,24 @@ void toggle_status_led(void)
     }
 }
 
+void toggle_status2(void)
+{
+    if (PORTC & 1<<PC4) {
+        PORTC &= ~(1<<PC4);
+    } else {
+        PORTC |= 1<<PC4;
+    }
+}
+
+void toggle_status3(void)
+{
+    if (PORTC & 1<<PC3) {
+        PORTC &= ~(1<<PC3);
+    } else {
+        PORTC |= 1<<PC3;
+    }
+}
+
 void toggle_heartbeat_led(void)
 {
     if (PORTC & 1<<PC2) {
--- util/pinfunctions.h
+++ util/pinfunctions.h
@@ -1,6 +1,8 @@
 #pragma once
 
 void toggle_status_led(void);
+void toggle_status2(void);
+void toggle_status3(void);
 void toggle_heartbeat_led(void);
 void enable_poweramp(void);
 void disable_poweramp(void);
