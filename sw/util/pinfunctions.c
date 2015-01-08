111a33e800ec4e0ff24327e2512aacb3e68e5b6d
Anton Eliasson <devel@antoneliasson.se>
2015-01-08 23:34:53 +0100

    Embarrassing cleanup

--- sw/util/pinfunctions.c
+++ sw/util/pinfunctions.c
@@ -3,11 +3,7 @@
 
 void toggle_status_led(void)
 {
-    if (PORTC & 1<<PC5) {
-        PORTC &= ~(1<<PC5);
-    } else {
-        PORTC |= 1<<PC5;
-    }
+    PORTC ^= _BV(PC5);
 }
 
 void set_status2(void)
@@ -17,37 +13,25 @@ void set_status2(void)
 
 void toggle_status2(void)
 {
-    if (PORTC & 1<<PC4) {
-        PORTC &= ~(1<<PC4);
-    } else {
-        PORTC |= 1<<PC4;
-    }
+    PORTC ^= _BV(PC4);
 }
 
 void toggle_status3(void)
 {
-    if (PORTC & 1<<PC3) {
-        PORTC &= ~(1<<PC3);
-    } else {
-        PORTC |= 1<<PC3;
-    }
+    PORTC ^= _BV(PC3);
 }
 
 void toggle_heartbeat_led(void)
 {
-    if (PORTC & 1<<PC2) {
-        PORTC &= ~(1<<PC2);
-    } else {
-        PORTC |= 1<<PC2;
-    }
+    PORTC ^= _BV(PC2);
 }
 
 void enable_poweramp(void)
 {
-    PORTD &= ~(1<<PD7);
+    PORTD &= ~_BV(PD7);
 }
 
 void disable_poweramp(void)
 {
-    PORTD |= 1<<PD7;
+    PORTD |= _BV(PD7);
 }
